1d8ca9346d8f6000d7c19c7fd977b0157135d2b7
Florin <fokokamga@yahoo.fr>
2019-06-26 14:18:36 +0200

    install firebase and other dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -52,10 +52,14 @@
  *
  */
 
+ import 'jquery';
+ import 'popper.js';
+ import 'bootstrap';
+ import 'firebase';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+ import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
