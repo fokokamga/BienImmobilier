166f314a87b41b8566632b43f0b5878bf9142c21
Florin <fokokamga@yahoo.fr>
2019-06-26 15:11:31 +0200

    config firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'maSuperAgence';
+
+  constructor() {
+    // Web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBcgveBprw9i673Tatx5e6wVQrW8P5DC8w',
+      authDomain: 'masuperagence-df6f1.firebaseapp.com',
+      databaseURL: 'https://masuperagence-df6f1.firebaseio.com',
+      projectId: 'masuperagence-df6f1',
+      storageBucket: '',
+      messagingSenderId: '653667171783',
+      appId: '1:653667171783:web:ad46bf2dc54c8cc5'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  }
 }
