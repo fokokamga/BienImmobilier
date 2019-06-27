892105d6e016e8cef4fb0937a3816d2bb39b2ee1
Florin <fokokamga@yahoo.fr>
2019-06-27 03:08:38 +0200

    authguard access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { AuthGuardService } from './services/auth-guard.service';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminSigninComponent} from './admin/admin-signin/admin-signin.component';
 
 const routes: Routes = [
+  // {path: 'home', component : HomeComponent},
+  // {path: 'property/:id', component : SinglePropertComponent},
   {path: 'admin/login', component : AdminSigninComponent},
-  {path: 'admin/dashboard', component : AdminDashboardComponent},
+  {path: 'admin/dashboard', canActivate: [AuthGuardService], component : AdminDashboardComponent},
+  {path: '', redirectTo: 'admin/login', pathMatch : 'full'},
+  {path: '**', redirectTo: 'admin/login'},
+
 
 ];
 
